60a1e969e9fdcd037933d64459f875ee26a7ddd6
CalvinL93 <65289606+CalvinL93@users.noreply.github.com>
2022-06-14 16:15:39 -0500

    first commit
    Minesweeper created from watching youtube video

---
+++ cell.py
@@ -0,0 +1,139 @@
+from tkinter import Button, Label
+import random
+import settings
+import ctypes
+import sys
+
+class Cell:
+    # List of all Cells
+    all = []
+    cell_count = settings.CELL_COUNT
+    cell_count_label_object = None
+    def __init__(self, x, y, is_mine=False):
+        self.is_mine = is_mine
+        self.is_opened = False
+        self.is_mine_candidate = False
+        self.cell_btn_object = None
+        self.x = x
+        self.y = y
+    
+        # Append the object to the Cell.all list
+        # Makes list of all cells created
+        Cell.all.append(self)
+        
+    def create_btn_object(self, location):
+        btn = Button(
+            location,
+            width=12, # adjust these numbers useing a formula for varied grid sizes
+            height=4, # ^^^^^^^^^
+            
+        )
+        btn.bind('<Button-1>', self.left_click_actions)
+        btn.bind('<Button-3>', self.right_click_actions)
+        self.cell_btn_object = btn
+        
+    @staticmethod    
+    def create_cell_count_label(location):
+        lbl = Label(
+            location,
+            bg = 'black',
+            fg = 'white',
+            text = f"Cells Left:{Cell.cell_count}",
+            font = ("", 30)
+        )
+        Cell.cell_count_label_object = lbl
+        
+    def left_click_actions(self, event): # event passed in because tkinter requires it. 
+        if self.is_mine:
+            self.show_mine()
+        else: # add a way to see connected zeros
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
+            self.show_cell()
+            # IF mine count is equal to cells left count, player wins
+            if Cell.cell_count == settings.MINES_COUNT:
+                ctypes.windll.user32.MessageBoxW(0, 'Congradulations! You won the game!', 'Game Over', 0) # last digit changes options from ok to yes no or others
+                sys.exit()
+                
+        # Cancel Left and Right click events if cell is already opened
+        self.cell_btn_object.unbind('<Button-1>')
+        self.cell_btn_object.unbind('<Button-3>')
+     
+    def get_cell_by_axis(self, x, y):
+        # Return a cell object based on the value of x, y
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+    
+    @property # makes function read only
+    def surrounded_cells(self):
+        cells = [ # try doing a loop
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y -1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+        
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+    
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+        
+        return counter
+
+    def show_cell(self):
+        if not self.is_opened:
+            Cell.cell_count -= 1
+            self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+            # Replace the text of cell count label with the newer count
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                    text=f"Cells Left:{Cell.cell_count}"    
+                )
+            # If this was a mine candidate, then we should configure
+            # the background color to SystemButton Face
+            self.cell_btn_object.configure(
+                bg = 'SystemButtonFace'
+            )
+        # Mark the cell as opened
+        self.is_opened = True
+            
+    def show_mine(self): # add functionaility to restart game
+        self.cell_btn_object.configure(bg = 'red')
+        ctypes.windll.user32.MessageBoxW(0, 'You clicked on a mine', 'Game Over', 0) # last digit changes options from ok to yes no or others
+        sys.exit()
+        
+    def right_click_actions(self, event): ###### add functionality for opened cells 
+        if not self.is_mine_candidate:
+            self.cell_btn_object.configure(
+                bg='orange'    
+            )
+            self.is_mine_candidate = True
+        else:
+            self.cell_btn_object.configure(
+                bg='SystemButtonFace' # try gray94 or F0F0F0
+            )
+            self.is_mine_candidate = False
+    
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(
+            Cell.all,
+            settings.MINES_COUNT
+        )
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+    
+    # Makes print(Cell.all) more readable when called
+    def __repr__(self):
+        return f"Cell({self.x}, {self.y})"
